5806b3366ef89295b56b3a1034123ad6dc2f6c34
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-04-14 21:04:28 +0900

    일시정지

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
+      ignoreEnvFile: process.env.NODE_ENV == 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),  자동으로 typescript 파일을 생성해준다.
