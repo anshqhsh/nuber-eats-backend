faa7ac7a9e66ba2becea9e998dd939e37cb09f86
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-04-28 23:33:39 +0900

    use joi validation check
    process.env의 변수 체크가 가능하다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
+import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { ApolloDriver } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
+console.log(Joi);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.test.env',
       ignoreEnvFile: process.env.NODE_ENV == 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -3,7 +3,7 @@ import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 // 리솔버를 사용하고 쿼리는 boolean을 리턴한다.
-// 레스토랑의 리솔버가 됨
+// 레스토랑의 리솔버가 됨 (entity)
 @Resolver((of) => Restaurant) // 직관적으로 표현하기 위함 () 이여도 됨
 export class RestaurantsResolver {
   @Query((returns) => [Restaurant]) // graphql 부분
